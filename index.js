a96d6cd0b2b5ef521625f0ed8062d6f007ea958c
JakeWalsh69 <jrwalsh1997@gmail.com>
2020-07-24 15:41:19 +0100

    THERE THERE THERE

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var options = ["Dam", "M.Base", "Quarry", "Airport", "TV Stat", "Lumber", "Stadium", "St. Town", "SuperS.", "Bone.", "Train S.", "Hosp.", 
 "Down.", "Farm.", "P. East", "P. West", "Hills", "Park", "Port", "Prison"];
 
+var options2 = ["Prison", "Prison", "Prison", "Clubs", "Prison", "Prison", "Prison", "Clubs", "Prison", "Prison", "Prison", "Clubs", 
+"Prison.", "Prison", "Prison", "Clubs", "Prison", "Prison", "Prison", "Clubs"];
+
 var startAngle = 0;
 var arc = Math.PI / (options.length / 2);
 var spinTimeout = null;
@@ -12,6 +15,7 @@ var spinTimeTotal = 0;
 var ctx;
 
 document.getElementById("spin").addEventListener("click", spin);
+document.getElementById("spin2").addEventListener("click", spin2);
 
 function byte2Hex(n) {
   var nybHexString = "0123456789ABCDEF";
@@ -90,6 +94,61 @@ function drawRouletteWheel() {
   }
 }
 
+function drawRouletteWheel2() {
+  var canvas2 = document.getElementById("canvas2");
+  if (canvas2.getContext) {
+    var outsideRadius = 200;
+    var textRadius = 160;
+    var insideRadius = 125;
+
+    ctx = canvas2.getContext("2d");
+    ctx.clearRect(0,0,500,500);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+
+    ctx.font = 'bold 12px Helvetica, Arial';
+
+    for(var i = 0; i < options2.length; i++) {
+      var angle = startAngle + i * arc;
+      //ctx.fillStyle = colors[i];
+      ctx.fillStyle = getColor(i, options2.length);
+
+      ctx.beginPath();
+      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+      ctx.stroke();
+      ctx.fill();
+
+      ctx.save();
+      ctx.shadowOffsetX = -1;
+      ctx.shadowOffsetY = -1;
+      ctx.shadowBlur    = 0;
+      ctx.shadowColor   = "rgb(220,220,220)";
+      ctx.fillStyle = "black";
+      ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius, 
+                    250 + Math.sin(angle + arc / 2) * textRadius);
+      ctx.rotate(angle + arc / 2 + Math.PI / 2);
+      var text = options2[i];
+      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+      ctx.restore();
+    } 
+
+    //Arrow
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
+    ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
+    ctx.fill();
+  }
+}
+
 function spin() {
   spinAngleStart = Math.random() * 10 + 10;
   spinTime = 0;
@@ -127,4 +186,42 @@ function easeOut(t, b, c, d) {
   return b+c*(tc + -3*ts + 3*t);
 }
 
+function spin2() {
+  spinAngleStart = Math.random() * 10 + 10;
+  spinTime = 0;
+  spinTimeTotal = Math.random() * 3 + 4 * 1000;
+  rotateWheel2();
+}
+
+function rotateWheel2() {
+  spinTime += 30;
+  if(spinTime >= spinTimeTotal) {
+    stopRotateWheel2();
+    return;
+  }
+  var spinAngle = spinAngleStart - easeOut2(spinTime, 0, spinAngleStart, spinTimeTotal);
+  startAngle += (spinAngle * Math.PI / 180);
+  drawRouletteWheel2();
+  spinTimeout = setTimeout('rotateWheel2()', 30);
+}
+
+function stopRotateWheel2() {
+  clearTimeout(spinTimeout);
+  var degrees = startAngle * 180 / Math.PI + 90;
+  var arcd = arc * 180 / Math.PI;
+  var index = Math.floor((360 - degrees % 360) / arcd);
+  ctx.save();
+  ctx.font = 'bold 30px Helvetica, Arial';
+  var text = options2[index]
+  ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
+  ctx.restore();
+}
+
+function easeOut2(t, b, c, d) {
+  var ts = (t/=d)*t;
+  var tc = ts*t;
+  return b+c*(tc + -3*ts + 3*t);
+}
+
 drawRouletteWheel();
+drawRouletteWheel2();
